d01d01ce3246dcf4613513a3babdd01eb621388c
ERIK M. DODD <emdodd27@gmail.com>
2022-12-20 17:09:53 -0800

    Finished the lucas sequence code section. tests were created and all passing. Updated the readMe for Attributions too

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,32 @@
 def fibonacci(n):
+    """
+    This function produces the nth value in the fibonacci sequence
+    using range to iterate over the sequence
+    """
     a, b = 0, 1
     for i in range(n):
         a, b = b, a + b
     return a
+
+
+def lucas(n):
+    """
+    This function produces the nth value in the lucas sequence
+    using range to iterate over the sequence
+    """
+    a, b = 2, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+
+def sum_series(n):
+    """
+    This function adds the sum of a series of numbers. For parameters,
+    it takes one required argument and two option ones.
+    """
+    
+    pass
+
 
 
